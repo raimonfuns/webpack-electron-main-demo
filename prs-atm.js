24bcf0325d21040eb5cefbc17e6bb858e6f24277
Leask Wong <i@leaskh.com>
2021-01-11 12:51:38 -0500

    tweak webpack script

--- prs-atm.js
+++ prs-atm.js
@@ -1,3 +1,6 @@
-const prsAtm = require('prs-atm');
+const prsAtm = require('prs-atm/dist');
 console.log(` ------------- output ---------------`);
 console.log(Object.keys(prsAtm));
+(async ()=> {
+    console.log(await prsAtm.producer.getAll());
+})();
--- webpack.config.prs-atm.js
+++ webpack.config.prs-atm.js
@@ -4,7 +4,9 @@ module.exports = {
 
   mode: 'production',
 
-  target: 'electron-main',
+  target: [
+    'electron12-main',
+  ],
 
   entry: './prs-atm.js',
 
@@ -14,11 +16,40 @@ module.exports = {
   },
 
   resolve: {
-    extensions: [".js", ".json", ".node"]
+    extensions: ['.js', '.json', '.node'],
+    alias: {
+      '@sentry/node': false,
+      'fast-geoip': false,
+      'ioredis': false,
+      'mailgun-js': false,
+      'mysql2/promise': false,
+      'node-mailjet': false,
+      'pg': false,
+      'public-ip': false,
+      'telesignsdk': false,
+      'twilio': false,
+      'winston-papertrail-mproved': false,
+      'winston': false,
+      'encoding': false,
+    }
   },
+
+  externals: [
+    /memcpy/, // eosjs-ecc
+    /pg-native/, // qg
+    /cardinal/, // mysql
+    /bufferutil/, // ws
+    /utf-8-validate/, // ws
+    {got: 'commonjs got' }, // public-ip
+  ],
+
+  ignoreWarnings: [
+
+  ],
 
   node: {
     __dirname: false,
     __filename: false,
   },
+
 };
